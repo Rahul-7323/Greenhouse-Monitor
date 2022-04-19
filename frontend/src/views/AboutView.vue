a60015616d69982d69a4271cb588a76438790073
Rahul M <roboto7o32oo3@gmail.com>
2022-04-19 16:21:07 +0530

    created vue app

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+     <h1 class="text-3xl font-bold underline text-slate-400">
+    Hello world!
+  </h1>
+  </div>
+  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
